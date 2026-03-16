28fad5cb51d194a9fedd0278e2e64914cd366254
Yehuda gavrieli <yehuda8009@gmail.com>
2026-03-16 12:55:45 +0200

    הוספתי הכל בגלל ההפסקת צהריים

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+from kafka import KafkaConsumer
 from students_part_1.simulator import run_simulator
 import my_engine
+import json
+
+
+consumer = KafkaConsumer("digital_hanter",
+        bootstrap_servers="localhost:9092",
+        value_serializer=lambda m:json.loads(m.decode("utf-8"))
+    )
 
 if __name__ == "__main__":
-    simulator = run_simulator(my_engine)
-    simulator.run()
+    print("the consumer running")
+    for message in consumer:
+        
+    run_simulator(my_engine)
--- my_engine.py
+++ my_engine.py
@@ -1,8 +1,18 @@
-from pymongo import Mongo_client
+from pymongo import MongoClient
 from logger import log_event
 
-client = Mongo_client("mongodb://localhost:27017/")
+client = MongoClient("mongodb://localhost:27017/")
 db = client["hunter_db"]
 collection1 = db["stations"]
 collection2 = db["measurements"]
 
+def add_station(id,lat,lon):
+    station_document = {"id":id , "lat":lat , "lon":lon}
+    db.collection1.insert_one(station_document)
+    print(f"{id} saved succeesfuly")
+
+
+data_station = db.collection1.find_one({id})
+
+if data_station:
+    
